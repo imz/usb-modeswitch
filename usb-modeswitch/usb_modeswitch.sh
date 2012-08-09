ab54fe927fa7de5c74ba9de2cd831b636c1fe938
Anton V. Boyarshinov <boyarsh@altlinux.org>
2012-08-09 17:50:55 +0400

    upstream 1.2.3

--- usb-modeswitch/usb_modeswitch.sh
+++ usb-modeswitch/usb_modeswitch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# part of usb_modeswitch 1.1.8
+# part of usb_modeswitch 1.2.3
 device_in()
 {
 	if [ ! -e /var/lib/usb_modeswitch/$1 ]; then
@@ -17,24 +17,32 @@ device_in()
 	return 0
 }
 
-p_id=$4
-if [ -z $p_id ]; then
-	prod=$5
-	if [ -z $prod ]; then
-		prod=$3
+if [ $(expr "$1" : "--.*") ]; then
+	p_id=$4
+	if [ -z $p_id ]; then
+		prod=$5
+		if [ -z $prod ]; then
+			prod=$3
+		fi
+		prod=${prod%/*}
+		v_id=0x${prod%/*}
+		p_id=0x${prod#*/}
+		if [ "$v_id" = "0x" ]; then
+			v_id="0"
+			p_id="0"
+		fi
+		v_id="$(printf %04x $(($v_id)))"
+		p_id="$(printf %04x $(($p_id)))"
+	else
+		v_id=$3
 	fi
-	prod=${prod%/*}
-	v_id=0x${prod%/*}
-	v_id=$(printf %04x $(($v_id)) )
-	p_id=0x${prod#*/}
-	p_id=$(printf %04x $(($p_id)) )
-else
-	v_id=$3
 fi
+PATH=/sbin:/usr/sbin:$PATH
 case "$1" in
 	--driver-bind)
 		(
 		dir=$(ls -d /sys$2/ttyUSB* 2>/dev/null)
+		sleep 1
 		if [ ! -z "$dir" ]; then
 			exit 0
 		fi
@@ -43,13 +51,13 @@ case "$1" in
 		if [ "$?" = "1" ]; then
 			id_attr="/sys/bus/usb-serial/drivers/option1/new_id"
 			if [ ! -e "$id_attr" ]; then
-				/sbin/modprobe option 2>/dev/null || true
+				modprobe option 2>/dev/null || true
 			fi
 			if [ -e "$id_attr" ]; then
-				echo "$v_id $p_id" > $id_attr
+				echo "$v_id $p_id ff" > $id_attr
 			else
-				/sbin/modprobe -r usbserial
-				/sbin/modprobe usbserial vendor=0x$v_id product=0x$p_id
+				modprobe -r usbserial
+				modprobe usbserial "vendor=0x$v_id" "product=0x$p_id"
 			fi
 		fi
 		) &
@@ -58,22 +66,24 @@ case "$1" in
 	--symlink-name)
 		device_in "link_list" $v_id $p_id
 		if [ "$?" = "1" ]; then
-			if [ -e "/usr/bin/tclsh" ]; then
-				exec /usr/bin/tclsh /usr/sbin/usb_modeswitch_dispatcher $1 $2 $v_id $p_id 2>/dev/null
+			if [ -e "/usr/sbin/usb_modeswitch_dispatcher" ]; then
+				exec usb_modeswitch_dispatcher $1 $2 2>>/dev/null
 			fi
 		fi
 		exit 0
 		;;
 esac
+exec 1<&- 2<&- 5<&- 7<&-
 (
 count=120
 while [ $count != 0 ]; do
-	if [ ! -e "/usr/bin/tclsh" ]; then
+	if [ ! -e "/usr/sbin/usb_modeswitch_dispatcher" ]; then
 		sleep 1
 		count=$(($count - 1))
 	else
-		exec /usr/bin/tclsh /usr/sbin/usb_modeswitch_dispatcher "$@" 2>/dev/null &
+		exec usb_modeswitch_dispatcher --switch-mode $1 $0 &
 		exit 0
 	fi
 done
 ) &
+exit 0
